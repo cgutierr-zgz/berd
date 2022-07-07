2eccb9361377d8df80c1a7cfd65994e952bd6cf8
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-07-07 14:28:35 +0200

    Example done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:berd/src/berd.dart';
 import 'package:flutter/material.dart';
 
@@ -14,13 +16,18 @@ void main() {
   ref.update<int>(number, (n) => n + 1);
   print(ref.watch(double, null)); // 22
   */
-  runApp(BerdCreatorGraph(child: const MyApp()));
+  runApp(BerdCreatorGraph(child: const BerdApp()));
 }
 
+// ignore: public_member_api_docs
 final counter = BerdCreator((ref, self) => 0);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// {@template berd_app}
+/// My Berd State Management App - roach.
+/// {@endtemplate}
+class BerdApp extends StatelessWidget {
+  /// {@macro berd_app}
+  const BerdApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -30,18 +37,108 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Berd State'),
         ),
-        body: Center(
-          child: BerdWatcher((context, ref, self) {
-            return Text('${ref.watch(counter, self)}');
-          }),
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Center(
+                child: BerdWatcher(
+                  (context, ref, self) {
+                    final count = ref.watch(counter, self);
+                    return Column(
+                      children: [
+                        Text('$count Berds'),
+                        Wrap(
+                          children: List.generate(count, (index) {
+                            final rand = Random().nextInt(Berds.values.length);
+                            final berd = Berds.values.elementAt(rand);
+
+                            return Image(
+                              image: AssetImage(berd.asset),
+                              height: 50,
+                            );
+                          }),
+                        )
+                      ],
+                    );
+                  },
+                ),
+              ),
+              const Positioned(
+                right: 70,
+                bottom: 50,
+                child: Text('Click here!'), // TODO(carlos): Comic Sans!
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Image(image: AssetImage(Berds.buyThatOne.asset)),
+              ),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             context.ref.update<int>(counter, (n) => n + 1);
           },
-          child: const Icon(Icons.add),
+          child: Image(image: AssetImage(Berds.white.asset)),
         ),
       ),
     );
   }
+}
+
+/// {@template berds}
+/// Enum with the list of all available berds
+/// {@endtemplate}
+enum Berds {
+  /// Blue berd
+  blue('${_path}blue.png'),
+
+  /// Cyan berd
+  cyan('${_path}cyan.png'),
+
+  /// Green berd
+  green('${_path}green.png'),
+
+  /// Orange berd
+  orange('${_path}orange.png'),
+
+  /// Purple berd
+  purple('${_path}purple.png'),
+
+  /// Red berd
+  red('${_path}red.png'),
+
+  /// White berd
+  white('${_path}white.png'),
+
+  /// Yellow berd
+  yellow('${_path}yellow.png'),
+
+  /// tiny blue berd as gif
+  tinyBlue('${_path}tinyblue.gif'),
+
+  /// tiny green berd as gif
+  tinyGreen('${_path}tinygreen.gif'),
+
+  /// tiny red berd as gif
+  tinyRed('${_path}tinyred.gif'),
+
+  /// tiny yellow berd as gif
+  tinyYellow('${_path}tinyyellow.gif'),
+
+  /// pointing berd as gif
+  buyThatOne('${_path}buy_that_one.gif'),
+
+  /// falling berd as gif
+  greeting('${_path}greeting.gif');
+
+  /// {@macro berds}
+  const Berds(this.asset);
+
+  static const _path = 'assets/images/';
+
+  /// Berd asset file
+  final String asset;
+
+  //String getPath(Berds berd) => '$_path${berd.asset}';
 }
--- lib/src/berd.dart
+++ lib/src/berd.dart
@@ -1,12 +1,24 @@
+// ignore_for_file: inference_failure_on_function_invocation
+
 import 'package:flutter/material.dart';
 
+/// {@template berd}
 /// Berd holds the state for creator
+/// {@endtemplate}
 class Berd<T> {
+  /// {@macro berd}
   Berd(this.ref, this.creator) : state = creator.create(ref, creator);
+
+  /// Berd's reference
   final Berdference ref;
+
+  /// Berd's creator
   final BerdCreator<T> creator;
+
+  /// Berd's state
   T state;
 
+  /// Recreates the current berd
   void recreate() {
     final newState = creator.create(ref, creator);
     if (newState != state) {
@@ -16,35 +28,43 @@ class Berd<T> {
   }
 }
 
-/// Creator creates a stream of T.
+/// {@template berd_creator}
+/// BerdCreator creates a stream of T.
+/// {@endtemplate}
 class BerdCreator<T> {
+  /// {@macro berd_creator}
   const BerdCreator(this.create);
+
+  /// Creates a Berd from a BerdReference
   final T Function(Berdference ref, BerdCreator<T> self) create;
   Berd<T> _createElement(Berdference ref) => Berd<T>(ref, this);
 }
 
-/// Ref holds the creator states and dependencies
+/// {@template berd_ref}
+/// Berdference holds the creator states and dependencies.
+/// {@endtemplate}
 class Berdference {
+  /// {@macro berd_ref}
   Berdference();
 
   /// Creator which holds state.
-  final Map<BerdCreator, Berd> _elements = {};
+  final Map<BerdCreator<dynamic>, Berd<dynamic>> _elements = {};
 
   /// Dependency graph. Think this as a directional graph.
   /// A -> [B, C] means if A changes, B and C need change too.
-  final Map<BerdCreator, Set<BerdCreator>> _graph = {};
+  final Map<BerdCreator<dynamic>, Set<BerdCreator<dynamic>>> _graph = {};
 
   /// Get or create an element for the given creator.
-  Berd _element<T>(BerdCreator creator) {
+  Berd<dynamic> _element<T>(BerdCreator<dynamic> creator) {
     return _elements.putIfAbsent(creator, () => creator._createElement(this));
   }
 
   /// Add an edge creator -> watcher to the graph, then return creator's state.
-  T watch<T>(BerdCreator<T> creator, BerdCreator? watcher) {
+  T watch<T>(BerdCreator<T> creator, BerdCreator<dynamic>? watcher) {
     if (watcher != null) {
       (_graph[creator] ??= {}).add(watcher);
     }
-    return _element<T>(creator).state;
+    return _element<T>(creator).state as T;
   }
 
   /// Set state of the creator.
@@ -58,19 +78,19 @@ class Berdference {
 
   /// Set state of creator using an update function. See [set]
   void update<T>(BerdCreator<T> creator, T Function(T) update) {
-    set<T>(creator, update(_element(creator).state));
+    set<T>(creator, update(_element(creator).state as T));
   }
 
   /// Propagate state changes.
-  void _onStateChange(BerdCreator creator) {
+  void _onStateChange(BerdCreator<dynamic> creator) {
     for (final c in _graph[creator] ?? {}) {
-      _element(c).recreate();
+      _element(c as BerdCreator).recreate();
     }
   }
 
   /// Delete the creator if it has no watcher. Also delete other creators who
   /// loses all their watchers.
-  void dispose(BerdCreator creator) {
+  void dispose(BerdCreator<dynamic> creator) {
     if ((_graph[creator] ?? {}).isNotEmpty) {
       return; // The creator is being watched by someone, cannot dispose it.
     }
@@ -92,15 +112,19 @@ class Berdference {
   }
 }
 
-/// Watcher is a widget which watches a creator and displays the creator's state.
-
+/// {@template berd_watcher}
 /// Watch creators to build a widget.
+/// {@endtemplate}
 class BerdWatcher extends StatefulWidget {
-  const BerdWatcher(this.builder, {Key? key}) : super(key: key);
+  /// {@macro berd_watcher}
+  const BerdWatcher(this.builder, {super.key});
 
   /// Allows watching creators to populate a widget.
   final Widget Function(
-      BuildContext context, Berdference ref, BerdCreator self)? builder;
+    BuildContext context,
+    Berdference ref,
+    BerdCreator<dynamic> self,
+  )? builder;
 
   @override
   State<BerdWatcher> createState() => _BerdWatcherState();
@@ -133,17 +157,25 @@ class _BerdWatcherState extends State<BerdWatcher> {
   }
 }
 
+/// {@template berd_extension}
+/// Allows access to a BerdReference from current context.
+/// {@endtemplate}
 extension ContextRef on BuildContext {
+  /// {@macro berd_extension}
   Berdference get ref => BerdCreatorGraph.of(this).ref;
 }
 
+/// {@template berd_c_graph}
+/// Simply expose Red trough context.
+/// {@endtemplate}
 class BerdCreatorGraph extends InheritedWidget {
-  BerdCreatorGraph({Key? key, required Widget child})
-      : ref = Berdference(),
-        super(key: key, child: child);
+  /// {@macro berd_c_graph}
+  BerdCreatorGraph({super.key, required super.child}) : ref = Berdference();
 
+  /// Current BerdReference.
   final Berdference ref;
 
+  /// {@macro berd_c_graph}
   static BerdCreatorGraph of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<BerdCreatorGraph>()!;
   }
